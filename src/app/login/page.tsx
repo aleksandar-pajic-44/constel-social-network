2b82b7b3cd1a9fbb35c81251e72318112099e280
Aleksandar Pajić <aleksandar.pajic@studiopresent.com>
2023-09-16 00:54:01 +0200

    [CO-01] Remove empty dependency array on login page, fix lint issue

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -31,7 +31,7 @@ export default function LoginPage() {
     if(cookies?.token) {
       router.push('/');
     }
-  }, []);
+  });
 
   const loginSubmit = async (loginCredentials: LoginCredentials) => {
     try {
